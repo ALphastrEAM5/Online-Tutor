37e3a83865f584335c35ebe2be8029b783c6969b
Abir Biswas <abirbiswas20023010@gmail.com>
2023-12-27 08:58:24 +0530

    commit

---
+++ server/utils/sendMail.ts
@@ -0,0 +1,40 @@
+require("dotenv").config()
+import nodeMailer, {Transporter} from 'nodemailer';
+import path from "path"
+import ejs from "ejs";
+
+interface EmailOptions{
+    email:string;
+    subject: string;
+    template: string;
+    data: {[key: string]: any};
+}
+
+const sendMail = async (options: EmailOptions): Promise <void> => {
+    const transporter: Transporter = nodeMailer.createTransport({
+        host: process.env.SMTP_HOST,
+        port: parseInt(process.env.SMTP_PORT || '587'),
+        service: process.env.SMTP_SERVICE,
+        auth:{
+            user: process.env.SMTP_MAIL,
+            pass: process.env.SMTP_PASSWORD,
+        },
+    });
+
+    const {email,subject,template,data} = options;
+
+    // get the path to the email template file
+    const templatePath = path.join(__dirname,"../mails", template);
+
+    // Render the email template with EJS
+     const html: string = await ejs.renderFile(templatePath, data);
+
+     const mailOptions = {
+        from: process.env.SMTP_MAIL,
+        to: email,
+        subject,
+        html
+     }
+}
+
+export default sendMail;
